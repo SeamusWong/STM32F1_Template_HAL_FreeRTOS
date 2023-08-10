8196842d76ff2cf591e5d6bd0d6b2ae2adefb2e8
SeamusWong <46064981+SeamusWong@users.noreply.github.com>
2023-08-10 21:55:30 +0800

    Update 2023.08.10

--- User/main.c
+++ User/main.c
@@ -6,15 +6,6 @@
 #include "whx_on_chip.h"
 
 #if ENABLE_FREERTOS
-/* FreeRTOS头文件 */
-#include "FreeRTOS.h"
-#include "task.h"
-#include "event_groups.h"
-#include "queue.h"
-#include "list.h"
-#include "timers.h"
-#include "event_groups.h"
-#include "croutine.h"
 /* 用户任务 */
 #include "whx_tasks.h"
 #else
@@ -23,11 +14,8 @@
 static void Initializer(void); /* 用于初始化板载相关资源 */
 
 #if ENABLE_FREERTOS
-/**
- * @description: FreeRTOS任务创建
- */
-static TaskHandle_t Handle_Task_Creator = NULL;
-static void Creator(void);
+static TaskHandle_t V_handle_task_Creator = NULL;
+static void Creator(void); /* 用于创建和初始化FreeRTOS中的所有任务、事件和信号量 */
 #else
 #endif
 
@@ -76,59 +64,61 @@ void SystemClockConfig(void)
  */
 static void Initializer(void)
 {
-  InitializeLED1();        /* LED初始化 */
-  InitializeKEY2();        /* 按键初始化 */
-  InitializeBEEP1();       /* 蜂鸣器初始化 */
-  InitializeUSART1();      /* USART通信初始化 */
-  InitializeEEPROM1();     /* EEPROM读写初始化 */
-  InitializeFLASH1();      /* FLASH读写初始化 */
-  InitializeLCD1();         /* LCD液晶屏幕初始化 */
+  /*外设初始化*/
+  InitializeLED1();    /* LED初始化 */
+  InitializeKEY2();    /* 按键初始化 */
+  InitializeBEEP1();   /* 蜂鸣器初始化 */
+  InitializeUSART1();  /* USART通信初始化 */
+  InitializeEEPROM1(); /* EEPROM读写初始化 */
+  // InitializeFLASH1();      /* FLASH读写初始化【FLASH接口和DAC有冲突，不要同时使用】 */
+  InitializeLCD1();        /* LCD液晶屏幕初始化 */
   InitializeADC1();        /* ADC电压读取初始化 */
+  InitializeDAC1();        /* DAC电压输出初始化【FLASH接口和DAC有冲突，不要同时使用】 */
   InitializeTIM6Base();    /* TIM6 基本定时器初始化 */
   InitializeTIM1Advance(); /* TIM1 PWM输出初始化 */
   InitializeTIM5General();
   InitializeCapacitiveButton1();
 
+  /*中断初始化*/
   // InitializeInterruptForGpio(1, 0);     /* 按键中断初始化	*/
   InitializeInterruptForADC1(1, 1);     /* ADC中断初始化 */
-  // InitializeInterruptForTIM6Base(1, 3); /* TIM6定时器中断初始化 */
+  InitializeInterruptForTIM6Base(1, 3); /* TIM6定时器中断初始化 */
   InitializeInterruptForTIM5General(1, 2);
   // InitializeInterruptForUSART(1,4); /* 通信中断初始化 */
 
+  /*中断开启*/
   // FuncInterruptForGpioEnable();
   // FuncInterruptForUSARTEnable();
   FuncInterruptForADC1Enable();
-  // FuncInterruptForTIM6BaseEnable();
+  FuncInterruptForTIM6BaseEnable();
   FuncInterruptForTIM5GeneralEnable();
 }
 
 int main(void)
 {
   HAL_Init();
-  SystemClockConfig();     /* 系统时钟初始化 */
+  SystemClockConfig(); /* 系统时钟初始化 */
 
   Initializer(); /* 开发板硬件初始化 */
 
 #if ENABLE_FREERTOS
-  BaseType_t status_task_create = pdPASS;                                 /* 定义一个创建信息返回值，默认为pdPASS */
-  status_task_create = xTaskCreate((TaskFunction_t)Creator,               /* 任务入口函数 */
-                                   (const char *)"Creator",               /* 任务名字 */
-                                   (uint16_t)512,                         /* 任务栈大小 */
-                                   (void *)NULL,                          /* 任务入口函数参数 */
-                                   (UBaseType_t)1,                        /* 任务的优先级 */
-                                   (TaskHandle_t *)&Handle_Task_Creator); /* 任务控制块指针 */
+  /***********************************************************************/
+  FUNC_CHECK_FOR_FREERTOS(xTaskCreate((TaskFunction_t)Creator,                 /* 任务入口函数 */
+                                      (const char *)"Creator",                 /* 任务名字 */
+                                      (uint16_t)512,                           /* 任务栈大小 */
+                                      (void *)NULL,                            /* 任务入口函数参数 */
+                                      (UBaseType_t)10,                         /* 任务的优先级 */
+                                      (TaskHandle_t *)&V_handle_task_Creator), /* 任务控制块指针 */
+                          "ERROR_TaskCreate_Creator");
+  /***********************************************************************/
 
-  if (pdPASS == status_task_create)
-  {
-    vTaskStartScheduler(); /* 启动任务，开启调度 */
-  }
-  else
-  {
-    FuncErrorAlert();
-  }
+  vTaskStartScheduler(); /* 启动任务，开启调度 */
+
 #else
 #endif
 
+  while (1)
+    ;
   return -1;
 }
 
@@ -136,85 +126,114 @@ int main(void)
 static void Creator(void)
 {
 
-  taskENTER_CRITICAL();                   // 进入临界区
-  BaseType_t status_task_create = pdPASS; /* 定义一个创建信息返回值，默认为pdPASS */
+  taskENTER_CRITICAL(); // 进入临界区
 
-  /***********************************************************************/
-  status_task_create = xTaskCreate((TaskFunction_t)TaskDeviceStart,           /* 任务入口函数 */
-                                   (const char *)"TaskDeviceStart",           /* 任务名字 */
-                                   (uint16_t)512,                             /* 任务栈大小 */
-                                   (void *)NULL,                              /* 任务入口函数参数 */
-                                   (UBaseType_t)10,                           /* 任务的优先级 */
-                                   (TaskHandle_t *)&V_handle_task_DeviceStart); /* 任务控制块指针 */
-  if (status_task_create != pdPASS)
-  {
-    FuncErrorAlert();
-  }
-  /***********************************************************************/
-  /***********************************************************************/
-  status_task_create = xTaskCreate((TaskFunction_t)TaskIdleLED,           /* 任务入口函数 */
-                                   (const char *)"TaskIdleLED",           /* 任务名字 */
-                                   (uint16_t)512,                         /* 任务栈大小 */
-                                   (void *)NULL,                          /* 任务入口函数参数 */
-                                   (UBaseType_t)1,                        /* 任务的优先级 */
-                                   (TaskHandle_t *)&V_handle_task_IdleLED); /* 任务控制块指针 */
-  if (status_task_create != pdPASS)
-  {
-    FuncErrorAlert();
-  }
-  /***********************************************************************/
-  /***********************************************************************/
-  status_task_create = xTaskCreate((TaskFunction_t)TaskScreenSystemTime,           /* 任务入口函数 */
-                                   (const char *)"TaskScreenSystemTime",           /* 任务名字 */
-                                   (uint16_t)512,                                  /* 任务栈大小 */
-                                   (void *)NULL,                                   /* 任务入口函数参数 */
-                                   (UBaseType_t)2,                                 /* 任务的优先级 */
-                                   (TaskHandle_t *)&V_handle_task_ScreenSystemTime); /* 任务控制块指针 */
-  if (status_task_create != pdPASS)
-  {
-    FuncErrorAlert();
-  }
-  /***********************************************************************/
-  /***********************************************************************/
-  status_task_create = xTaskCreate((TaskFunction_t)TaskScreenShowInfo,           /* 任务入口函数 */
-                                   (const char *)"TaskScreenShowInfo",           /* 任务名字 */
-                                   (uint16_t)512,                                /* 任务栈大小 */
-                                   (void *)NULL,                                 /* 任务入口函数参数 */
-                                   (UBaseType_t)2,                               /* 任务的优先级 */
-                                   (TaskHandle_t *)&V_handle_task_ScreenShowInfo); /* 任务控制块指针 */
-  if (status_task_create != pdPASS)
-  {
-    FuncErrorAlert();
-  }
-  /***********************************************************************/
-  /***********************************************************************/
-  status_task_create = xTaskCreate((TaskFunction_t)TaskCapacitiveButtonLED,             /* 任务入口函数 */
-                                   (const char *)"TaskCapacitiveButtonLED",             /* 任务名字 */
-                                   (uint16_t)512,                                       /* 任务栈大小 */
-                                   (void *)NULL,                                        /* 任务入口函数参数 */
-                                   (UBaseType_t)3,                                      /* 任务的优先级 */
-                                   (TaskHandle_t *)&V_handle_task_CapacitiveButtonLED); /* 任务控制块指针 */
-  if (status_task_create != pdPASS)
-  {
-    FuncErrorAlert();
-  }
-  /***********************************************************************/
-  /***********************************************************************/
-  status_task_create = xTaskCreate((TaskFunction_t)TaskScreenChangeByKey,           /* 任务入口函数 */
-                                   (const char *)"TaskScreenChangeByKey",           /* 任务名字 */
-                                   (uint16_t)512,                                   /* 任务栈大小 */
-                                   (void *)NULL,                                    /* 任务入口函数参数 */
-                                   (UBaseType_t)3,                                  /* 任务的优先级 */
-                                   (TaskHandle_t *)&V_handle_task_ScreenChangeByKey); /* 任务控制块指针 */
-  if (status_task_create != pdPASS)
-  {
-    FuncErrorAlert();
-  }
-  /***********************************************************************/
+  /**
+   * @description: 任务创建区
+   */
+  FUNC_CHECK_FOR_FREERTOS(xTaskCreate((TaskFunction_t)TaskDeviceStart,             /* 任务入口函数 */
+                                      (const char *)"TaskDeviceStart",             /* 任务名字 */
+                                      (uint16_t)512,                               /* 任务栈大小 */
+                                      (void *)NULL,                                /* 任务入口函数参数 */
+                                      (UBaseType_t)10,                             /* 任务的优先级 */
+                                      (TaskHandle_t *)&V_handle_task_DeviceStart), /* 任务控制块指针 */
+                          "ERROR_TaskCreate_TaskDeviceStart");
+  FUNC_CHECK_FOR_FREERTOS(xTaskCreate((TaskFunction_t)TaskIdleLED,             /* 任务入口函数 */
+                                      (const char *)"TaskIdleLED",             /* 任务名字 */
+                                      (uint16_t)512,                           /* 任务栈大小 */
+                                      (void *)NULL,                            /* 任务入口函数参数 */
+                                      (UBaseType_t)1,                          /* 任务的优先级 */
+                                      (TaskHandle_t *)&V_handle_task_IdleLED), /* 任务控制块指针 */
+                          "ERROR_TaskCreate_TaskIdleLED");
+  FUNC_CHECK_FOR_FREERTOS(xTaskCreate((TaskFunction_t)TaskScreenShowInfo,             /* 任务入口函数 */
+                                      (const char *)"TaskScreenShowInfo",             /* 任务名字 */
+                                      (uint16_t)512,                                  /* 任务栈大小 */
+                                      (void *)NULL,                                   /* 任务入口函数参数 */
+                                      (UBaseType_t)2,                                 /* 任务的优先级 */
+                                      (TaskHandle_t *)&V_handle_task_ScreenShowInfo), /* 任务控制块指针 */
+                          "ERROR_TaskCreate_TaskScreenShowInfo");
+  FUNC_CHECK_FOR_FREERTOS(xTaskCreate((TaskFunction_t)TaskScreenSystemTime,             /* 任务入口函数 */
+                                      (const char *)"TaskScreenSystemTime",             /* 任务名字 */
+                                      (uint16_t)512,                                    /* 任务栈大小 */
+                                      (void *)NULL,                                     /* 任务入口函数参数 */
+                                      (UBaseType_t)3,                                   /* 任务的优先级 */
+                                      (TaskHandle_t *)&V_handle_task_ScreenSystemTime), /* 任务控制块指针 */
+                          "ERROR_TaskCreate_TaskScreenSystemTime");
+  FUNC_CHECK_FOR_FREERTOS(xTaskCreate((TaskFunction_t)TaskCapacitiveButtonLED,             /* 任务入口函数 */
+                                      (const char *)"TaskCapacitiveButtonLED",             /* 任务名字 */
+                                      (uint16_t)512,                                       /* 任务栈大小 */
+                                      (void *)NULL,                                        /* 任务入口函数参数 */
+                                      (UBaseType_t)3,                                      /* 任务的优先级 */
+                                      (TaskHandle_t *)&V_handle_task_CapacitiveButtonLED), /* 任务控制块指针 */
+                          "ERROR_TaskCreate_TaskCapacitiveButtonLED");
+  FUNC_CHECK_FOR_FREERTOS(xTaskCreate((TaskFunction_t)TaskTim5General,             /* 任务入口函数 */
+                                      (const char *)"TaskTim5General",             /* 任务名字 */
+                                      (uint16_t)512,                               /* 任务栈大小 */
+                                      (void *)NULL,                                /* 任务入口函数参数 */
+                                      (UBaseType_t)3,                              /* 任务的优先级 */
+                                      (TaskHandle_t *)&V_handle_task_Tim5General), /* 任务控制块指针 */
+                          "ERROR_TaskCreate_TaskTim5General");
+  FUNC_CHECK_FOR_FREERTOS(xTaskCreate((TaskFunction_t)TaskADC,             /* 任务入口函数 */
+                                      (const char *)"TaskADC",             /* 任务名字 */
+                                      (uint16_t)512,                       /* 任务栈大小 */
+                                      (void *)NULL,                        /* 任务入口函数参数 */
+                                      (UBaseType_t)3,                      /* 任务的优先级 */
+                                      (TaskHandle_t *)&V_handle_task_ADC), /* 任务控制块指针 */
+                          "ERROR_TaskCreate_TaskADC");
+  /***********************************任务创建区***********************************/
 
-  vTaskDelete(Handle_Task_Creator); // 删除AppTaskCreate任务
-  taskEXIT_CRITICAL();              // 退出临界区
+  /**
+   * @description: 消息队列创建区
+   */
+  V_handle_queue_screen_adc = xQueueCreate(5, sizeof(Type_Queue_Screen_ADC));
+  if (V_handle_queue_screen_adc == NULL)
+  {
+    printf("ERROR_QueueCreate_V_handle_queue_screen_adc\n");
+    FuncErrorAlert();
+  }
+
+  V_handle_queue_screen_capacitivebutton = xQueueCreate(5, sizeof(Type_Queue_Screen_CapacitiveButton));
+  if (V_handle_queue_screen_adc == NULL)
+  {
+    printf("ERROR_QueueCreate_V_handle_queue_screen_capacitivebutton\n");
+    FuncErrorAlert();
+  }
+
+  V_handle_queue_screen_tim5general = xQueueCreate(5, sizeof(Type_Queue_Screen_Tim5General));
+  if (V_handle_queue_screen_adc == NULL)
+  {
+    printf("ERROR_QueueCreate_V_handle_queue_screen_tim5general\n");
+    FuncErrorAlert();
+  }
+
+  V_handle_queue_screen_systemtime = xQueueCreate(5, sizeof(Type_Queue_Screen_SystemTime));
+  if (V_handle_queue_screen_systemtime == NULL)
+  {
+    printf("ERROR_QueueCreate_V_handle_queue_screen_systemtime\n");
+    FuncErrorAlert();
+  }
+  /*********************************消息队列创建区*********************************/
+
+  /**
+   * @description: 事件组创建区
+   */
+  V_handle_event_screen_refresh = xEventGroupCreate();
+  if (V_handle_event_screen_refresh == NULL)
+  {
+    printf("ERROR_EventCreate_V_handle_event_screen_refresh\n");
+    FuncErrorAlert();
+  }
+  /*********************************事件组创建区*********************************/
+
+  /**
+   * @description: Others
+   */
+
+  /*********************************Others*********************************/
+
+  vTaskDelete(V_handle_task_Creator); // 删除Creator任务
+  taskEXIT_CRITICAL();                // 退出临界区
 }
 #else
 #endif
-/********************************END OF FILE****************************/
+/****************************END OF FILE****************************/
