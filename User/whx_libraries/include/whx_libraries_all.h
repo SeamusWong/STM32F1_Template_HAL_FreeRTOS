97276f229b4beb6f214a0f05da18429bf3762ba9
SeamusWong <46064981+SeamusWong@users.noreply.github.com>
2023-08-10 21:52:27 +0800

    初始引用库 Update 2023.08.10

--- User/whx_libraries/include/whx_libraries_all.h
+++ User/whx_libraries/include/whx_libraries_all.h
@@ -4,7 +4,7 @@
 #include <stm32f1xx_hal.h>
 #include <stdarg.h>
 
-#define ENABLE_FREERTOS 1 /*启用FreeRTOS内核*/
+#define ENABLE_FREERTOS 1 /*是否启用FreeRTOS内核，禁用后所有和FreeRTOS有关的代码全部失效*/
 
 #if ENABLE_FREERTOS
 /* FreeRTOS头文件 */
@@ -16,15 +16,24 @@
 #include "timers.h"
 #include "event_groups.h"
 #include "croutine.h"
+#include "semphr.h"
 #else
 #endif
 
 #include "GB2312_Font.h"
 
+/*下方是在本库中随处用到的最基本量和函数*/
+
 #define NO_USE NULL /*仅用在配置宏定义中，表示这项配置没有使用，仅在代码当中起提示和便于学习作用*/
 
+/**
+ * @description: 错误报警函数，需要外部实现，不能退出，进入了就表示你又要改BUG了
+ */
 extern void FuncErrorAlert(void);
 
+/**
+ * @description: 用于HAL库中各处返回值必须是HAL_OK的校验，不是就报错
+ */
 #define FUNC_CHECK_FOR_HAL(m_func, m_info) \
     do                                     \
     {                                      \
@@ -37,6 +46,10 @@ extern void FuncErrorAlert(void);
     } while (0U);
 
 #if ENABLE_FREERTOS
+
+/**
+ * @description: 用于FreeRTOS中各处返回值必须是pdPASS的校验，不是就报错
+ */
 #define FUNC_CHECK_FOR_FREERTOS(m_func, m_info) \
     do                                          \
     {                                           \
@@ -50,6 +63,9 @@ extern void FuncErrorAlert(void);
 #else
 #endif
 
+/**
+ * @description: 在一段毫秒时间内等待一个值，超时了就报错
+ */
 #define FUNC_WAITSTATUS_TIMEOUT(m_func, m_status_wait, m_timeout_ms, m_info)            \
     do                                                                                  \
     {                                                                                   \
@@ -65,6 +81,9 @@ extern void FuncErrorAlert(void);
         }                                                                               \
     } while (0U);
 
+/**
+ * @description: 一个微秒级的延时，可以用在任何地方
+ */
 #define FUNC_DELAY_US_FOR_ALL(m_time_us)                                              \
     {                                                                                 \
         uint32_t delay = (uint32_t)m_time_us * (HAL_RCC_GetSysClockFreq() / 1000000); \
